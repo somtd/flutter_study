f25ab4bb5acc3a28b71b50e314504c2f5b0d9d42
somtd <somtd1104@me.com>
2019-05-19 22:40:08 +0900

    Merged user and products file

--- lib/scoped_models/connected_products.dart
+++ lib/scoped_models/connected_products.dart
@@ -3,10 +3,10 @@ import 'package:scoped_model/scoped_model.dart';
 import '../models/product.dart';
 import '../models/user.dart';
 
-mixin ConnectedProducts on Model {
-  List<Product> products = [];
-  int selProductIndex;
-  User authenticatedUser;
+mixin ConnectedProductsModel on Model {
+  List<Product> _products = [];
+  int _selProductIndex;
+  User _authenticatedUser;
 
   void addProduct(
       String title, String description, String image, double price) {
@@ -15,11 +15,96 @@ mixin ConnectedProducts on Model {
       description: description,
       image: image,
       price: price,
-      userEmail: authenticatedUser.email,
-      userId: authenticatedUser.id,
+      userEmail: _authenticatedUser.email,
+      userId: _authenticatedUser.id,
     );
-    products.add(newProduct);
-    selProductIndex = null;
+    _products.add(newProduct);
+    _selProductIndex = null;
     notifyListeners();
+  }
+}
+
+mixin ProductsModel on ConnectedProductsModel {
+  bool _showFavorites = false;
+
+  List<Product> get allProducts {
+    // 参照ではなく、コピーを返す（Newのようなもの）
+    return List.from(_products);
+  }
+
+  List<Product> get displayedProducts {
+    if (_showFavorites) {
+      return _products.where((Product product) => product.isFavorite).toList();
+    }
+    return List.from(_products);
+  }
+
+  int get selectedProductIndex {
+    return _selProductIndex;
+  }
+
+  Product get selectedProduct {
+    return selectedProductIndex == null
+        ? null
+        : _products[selectedProductIndex];
+  }
+
+  bool get displayFavoritesOnly {
+    return _showFavorites;
+  }
+
+  void updateProduct(
+      String title, String description, String image, double price) {
+    final updatedProduct = Product(
+      title: title,
+      description: description,
+      image: image,
+      price: price,
+      userEmail: selectedProduct.userEmail,
+      userId: selectedProduct.userId,
+    );
+    _products[selectedProductIndex] = updatedProduct;
+    _selProductIndex = null;
+    notifyListeners();
+  }
+
+  void deleteProduct() {
+    _products.removeAt(selectedProductIndex);
+    _selProductIndex = null;
+    notifyListeners();
+  }
+
+  void toggleProductFavoriteStatus() {
+    final bool isProductFavorite = selectedProduct.isFavorite;
+    final bool newFavoriteStatus = !isProductFavorite;
+    final Product updatedProduct = Product(
+        title: selectedProduct.title,
+        description: selectedProduct.description,
+        price: selectedProduct.price,
+        image: selectedProduct.image,
+        userEmail: selectedProduct.userEmail,
+        userId: selectedProduct.userId,
+        isFavorite: newFavoriteStatus);
+    _products[selectedProductIndex] = updatedProduct;
+    // 全てのScopedModelに変更を通知して、再描画させる。
+    notifyListeners();
+    _selProductIndex = null;
+  }
+
+  void selectProduct(int index) {
+    _selProductIndex = index;
+    notifyListeners();
+  }
+
+  void toggleDisplayMode() {
+    _showFavorites = !_showFavorites;
+    notifyListeners();
+  }
+}
+
+mixin UserModel on ConnectedProductsModel {
+  void login(String email, String password) {
+    _authenticatedUser =
+        User(id: 'retsgyetee', email: email, password: password);
   }
 }
--- lib/scoped_models/user.dart
+++
@@ -1,9 +0,0 @@
-import '../models/user.dart';
-import '../scoped_models/connected_products.dart';
-
-mixin UserModel on ConnectedProducts {
-  void login(String email, String password) {
-    authenticatedUser =
-        User(id: 'retsgyetee', email: email, password: password);
-  }
-}
