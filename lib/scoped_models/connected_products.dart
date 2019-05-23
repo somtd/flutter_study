b471fc32e429722a880924357f7bf5974cbc4e18
somtd <somtd1104@me.com>
2019-05-23 22:49:13 +0900

    Adding http and post method

--- lib/scoped_models/connected_products.dart
+++ lib/scoped_models/connected_products.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:scoped_model/scoped_model.dart';
+import 'package:http/http.dart' as http;
 
 import '../models/product.dart';
 import '../models/user.dart';
@@ -10,6 +13,14 @@ mixin ConnectedProductsModel on Model {
 
   void addProduct(
       String title, String description, String image, double price) {
+    final Map<String, dynamic> productData = {
+      'title': title,
+      'description': description,
+      'image': 'https://www.mary.co.jp/mary/images/topics/img_180402_main.jpg',
+      'price': price,
+    };
+    http.post('https://flutter-products-f2789.firebaseio.com/products.json',
+        body: json.encode(productData));
     final newProduct = Product(
       title: title,
       description: description,
