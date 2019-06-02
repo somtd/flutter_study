a390ea081b4cd7ea11ee5cfaddd6fb6b390b2c08
somtd <somtd1104@me.com>
2019-06-02 23:41:33 +0900

    Adjusting selected product

--- lib/pages/product_edit.dart
+++ lib/pages/product_edit.dart
@@ -102,7 +102,7 @@ class _ProductEditPageState extends State<ProductEditPage> {
       return;
     }
     _formKey.currentState.save();
-    if (selectedProductIndex == null) {
+    if (selectedProductIndex == -1) {
       addProduct(
         _formData['title'],
         _formData['description'],
@@ -157,7 +157,7 @@ class _ProductEditPageState extends State<ProductEditPage> {
         builder: (BuildContext context, Widget child, MainModel model) {
       final Widget pageContent =
           _buildPageContent(context, model.selectedProduct);
-      return model.selectedProductIndex == null
+      return model.selectedProductIndex == -1
           ? pageContent
           : Scaffold(
               appBar: AppBar(
